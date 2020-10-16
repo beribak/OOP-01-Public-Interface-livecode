3b28863f145c1db12d98577a97eda64252981e90
Danko Beribak <beribakdanko@gmail.com>
2020-10-15 21:32:04 -0700

    annotate test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -1,12 +1,9 @@
-
-
-
 require_relative "bankaccount.rb"
 
 account = BankAccount.new("John Lennon", "FR14-2004-1010-0505-0001-3M02-606", 200, "yoko")
 
-# puts account.iban
-# puts account.to_s
-puts account.transaction_history({password: "yoko"})
-puts account.transaction_history()
-puts account.transaction_history({password: "yokw"})
+puts account.iban # => "FR14**************606"
+puts account.to_s  # => Owner: John Lennon - IBAN: FR14**************606 - Balance: 200 euros
+puts account.transaction_history({password: "yoko"}) # => "t1" "t2"
+puts account.transaction_history() # => "You have to enter a password!!!"
+puts account.transaction_history({password: "yokw"}) # => "Wrong password!!!"
